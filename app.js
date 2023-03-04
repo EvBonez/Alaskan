2a3ab9ac212a788427fc61131a277c34ca36e9d9
Evan C <connollycevan@gmail.com>
2023-03-04 14:25:20 -0500

    Renders decks and number of cards remaining in each deck

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs');
 var deck1Index = 2;
 var deck2Index = 2;
 
+var deck1NumberOfCards = 50;
+var deck2NumberOfCards = 50;
+
 const deck1 = [];
 const deck2 = [];
 
@@ -48,6 +51,8 @@ app.get("/", function (req, res) {
         p1SecondCard:p1SecondCard.value+p1SecondCard.suit,
         p2FirstCard:p2FirstCard.value+p2FirstCard.suit,
         p2SecondCard:p2SecondCard.value+p2SecondCard.suit,
+        deck1NumberOfCards:deck1NumberOfCards,
+        deck2NumberOfCards:deck2NumberOfCards,
         });
 });
 
@@ -57,21 +62,25 @@ app.post("/", function (req, res) {
         let playValue = p1FirstCard.valueValues
         p1FirstCard = deck1[deck1Index];
         deck1Index+=playValue
+        deck1NumberOfCards-=playValue
     }
     if(req.body.hasOwnProperty("p1SecondCard-button")){
         let playValue = p1SecondCard.valueValues
         p1SecondCard = deck1[deck1Index];
         deck1Index+=playValue
+        deck1NumberOfCards-=playValue
     }
     if(req.body.hasOwnProperty("p2FirstCard-button")){
         let playValue = p2FirstCard.valueValues
         p2FirstCard = deck2[deck2Index];
         deck2Index+=playValue
+        deck2NumberOfCards-=playValue
     }
     if(req.body.hasOwnProperty("p2SecondCard-button")){
         let playValue = p2SecondCard.valueValues
         p2SecondCard = deck2[deck2Index];
         deck2Index+=playValue
+        deck2NumberOfCards-=playValue
     }
     res.redirect("/")
 });
