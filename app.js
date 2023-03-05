395a3c42f8efbc4474da2f425847f53590aac571
Evan C <connollycevan@gmail.com>
2023-03-04 22:21:35 -0500

    adds community cards

--- app.js
+++ app.js
@@ -17,9 +17,11 @@ var deck2NumberOfCards = 48;
 
 const deck1 = [];
 const deck2 = [];
+const deckBoard = [];
 
 var p1Hand = [];
 var p2Hand = [];
+var board = [];
 
 var p1FirstCard = ""
 var p1SecondCard = ""
@@ -31,10 +33,17 @@ var p2SecondCard = ""
 var p2ThirdCard = ""
 var p2FourthCard = ""
 
+var boardCard1= ""
+var boardCard2= ""
+var boardCard3= ""
+var boardCard4= ""
+var boardCard5= ""
+
 //game logic
 function gameStart() {
     decks.makeDeck(deck1);
     decks.makeDeck(deck2);
+    decks.makeDeck(deckBoard);
 
     p1Hand = [deck1[0], deck1[1], deck1[2], deck1[3]];
     p2Hand = [deck2[0], deck2[1], deck2[2], deck2[3]];
@@ -46,6 +55,11 @@ function gameStart() {
     p2SecondCard = p2Hand[1];
     p2ThirdCard = p2Hand[2];
     p2FourthCard = p2Hand[3];
+    boardCard1 = deckBoard[1]
+    boardCard2 = deckBoard[2]
+    boardCard3 = deckBoard[3]
+    boardCard4 = deckBoard[5]
+    boardCard5 = deckBoard[7]
 }
 
 
@@ -65,6 +79,11 @@ app.get("/", function (req, res) {
         p2FourthCard:p2FourthCard.value+p2FourthCard.suit,
         deck1NumberOfCards:deck1NumberOfCards,
         deck2NumberOfCards:deck2NumberOfCards,
+        boardCard1: boardCard1.value+boardCard1.suit,
+        boardCard2: boardCard2.value+boardCard2.suit,
+        boardCard3: boardCard3.value+boardCard3.suit,
+        boardCard4: boardCard4.value+boardCard4.suit,
+        boardCard5: boardCard5.value+boardCard5.suit,
         });
 });
 
@@ -76,43 +95,43 @@ app.post("/", function (req, res) {
         deck1Index+=playValue
         deck1NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p1SecondCard-button")){
+    else if(req.body.hasOwnProperty("p1SecondCard-button")){
         let playValue = p1SecondCard.valueValues
         p1SecondCard = deck1[deck1Index];
         deck1Index+=playValue
         deck1NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p1ThirdCard-button")){
+    else if(req.body.hasOwnProperty("p1ThirdCard-button")){
         let playValue = p1ThirdCard.valueValues
         p1ThirdCard = deck1[deck1Index];
         deck1Index+=playValue
         deck1NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p1FourthCard-button")){
+    else if(req.body.hasOwnProperty("p1FourthCard-button")){
         let playValue = p1FourthCard.valueValues
         p1FourthCard = deck1[deck1Index];
         deck1Index+=playValue
         deck1NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p2FirstCard-button")){
+    else if(req.body.hasOwnProperty("p2FirstCard-button")){
         let playValue = p2FirstCard.valueValues
         p2FirstCard = deck2[deck2Index];
         deck2Index+=playValue
         deck2NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p2SecondCard-button")){
+    else if(req.body.hasOwnProperty("p2SecondCard-button")){
         let playValue = p2SecondCard.valueValues
         p2SecondCard = deck2[deck2Index];
         deck2Index+=playValue
         deck2NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p2ThirdCard-button")){
+    else if(req.body.hasOwnProperty("p2ThirdCard-button")){
         let playValue = p2ThirdCard.valueValues
         p2ThirdCard = deck2[deck2Index];
         deck2Index+=playValue
         deck2NumberOfCards-=playValue
     }
-    if(req.body.hasOwnProperty("p2FourthCard-button")){
+    else if(req.body.hasOwnProperty("p2FourthCard-button")){
         let playValue = p2FourthCard.valueValues
         p2FourthCard = deck2[deck2Index];
         deck2Index+=playValue
